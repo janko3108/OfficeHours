d7002da784a2cf6ca5a62c9357ae398c97e691b9
Jleskovac <jl8592@rit.edu>
2025-03-11 15:06:46 +0100

    fixed not working on laptop

--- office_scheduler/urls.py
+++ office_scheduler/urls.py
@@ -1,14 +1,22 @@
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
 from django.shortcuts import redirect
 from scheduler import views as scheduler_views
+from two_factor import urls as two_factor_urls_module
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Include two_factor URLs without an extra "account/" prefix.
-    path('', include('two_factor.urls')),
     
-    # If you want /login/ to redirect to the two_factor login, do this:
+    # Unpack the two_factor URLconf tuple:
+    path(
+        '', 
+        include(
+            (two_factor_urls_module.urlpatterns[0], two_factor_urls_module.urlpatterns[1]),
+            namespace='two_factor'
+        )
+    ),
+    
+    # Redirect /login/ to the two_factor login URL.
     path('login/', lambda request: redirect('two_factor:login'), name='login'),
     
     # Home view and your app's URLs:
